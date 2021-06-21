5ba496985ea379e3de38c895c43e269c01dd0e54
大島進太朗(s-oshima) <s-oshima@jtgp.local>
2021-06-21 09:43:08 +0900

    update index.js

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ exports.render = function (text='', options={}) {
   let rtext = "";
   if(options.minify){
     rtext = `<?php ${text.replace(/\s+/g, '')} ?>`;
+    rtext = `${rtext.replace(/\<script/g, '<script ')}`;
+    rtext = `${rtext.replace(/\<link/g, '<link ')}`;
   }else{
     rtext = `\n<?php\n${text}\n?>\n`;
   }
